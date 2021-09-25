5c1b801575a4bd49e515cb1c6aaa3d3dd2e027ea
hardow2011 <hardow2011@outlook.com>
2021-09-25 07:39:32 -0400

    Project created

---
+++ src/main/java/com/example/crudsimple/CrudSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSimpleApplication.class, args);
+    }
+
+}
